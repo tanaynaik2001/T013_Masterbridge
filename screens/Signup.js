eb289b49bc802f394a4a5fb50064778d99515fb3
Tanay Naik <tanayn945@gmail.com>
2020-09-27 10:21:07 +0530

    Updated Instructor Course Detail

--- screens/Signup.js
+++ screens/Signup.js
@@ -1,12 +1,5 @@
 import React, {useState} from 'react';
-import {
-  StyleSheet,
-  Text,
-  View,
-  TextInput,
-  Dimensions,
-  Alert,
-} from 'react-native';
+import {StyleSheet, Text, View, TextInput, ScrollView} from 'react-native';
 import {Card, Button, RadioButton} from 'react-native-paper';
 import {useNavigation} from '@react-navigation/native';
 import {firebase} from '../firebase/config';
@@ -49,55 +42,76 @@ const Signup = () => {
       });
   };
   return (
-    <View style={styles.container}>
-      <Card style={styles.cardContainer}>
-        <Card.Content>
-          <Card.Title title="Name" />
-          <TextInput
-            placeholder="Enter your Name"
-            style={styles.inputContainer}
-            value={name}
-            onChangeText={(text) => setName(text)}
-          />
-          <Card.Title title="Email" />
-          <TextInput
-            placeholder="Enter your valid email address"
-            style={styles.inputContainer}
-            value={email}
-            onChangeText={(text) => setEmail(text)}
-          />
-          <Card.Title title="Password" />
-          <TextInput
-            placeholder="Enter password"
-            style={styles.inputContainer}
-            secureTextEntry
-            value={password}
-            onChangeText={(text) => setPassword(text)}
-          />
+    <ScrollView>
+      <View style={styles.container}>
+        <Card style={styles.cardContainer}>
+          <Card.Content>
+            <Card.Title title="Name" />
+            <TextInput
+              placeholder="Enter your Name"
+              style={styles.inputContainer}
+              value={name}
+              onChangeText={(text) => setName(text)}
+            />
+            <Card.Title title="Email" />
+            <TextInput
+              placeholder="Enter your valid email address"
+              style={styles.inputContainer}
+              value={email}
+              onChangeText={(text) => setEmail(text)}
+            />
+            <Card.Title title="Password" />
+            <TextInput
+              placeholder="Enter password"
+              style={styles.inputContainer}
+              secureTextEntry
+              value={password}
+              onChangeText={(text) => setPassword(text)}
+            />
 
-          <Card.Title title="You are?" />
-          <RadioButton.Group
-            value={value}
-            onValueChange={(text) => setValue(text)}>
-            <View style={styles.radBtn}>
-              <Text>Student</Text>
-              <RadioButton value="Student" color="red" />
-            </View>
-            <View style={{...styles.radBtn, marginHorizontal: 6}}>
-              <Text>Instructor</Text>
-              <RadioButton value="Instructor" color="red" />
-            </View>
-          </RadioButton.Group>
-          <Button
-            color="black"
-            mode="outlined"
-            style={styles.btn}
-            onPress={() => onRegisterPress()}>
-            Submit
-          </Button>
-        </Card.Content>
-      </Card>
-    </View>
+            <Card.Title title="You are?" />
+            <RadioButton.Group
+              value={value}
+              onValueChange={(text) => setValue(text)}>
+              <View style={styles.radBtn}>
+                <Text>Student</Text>
+                <RadioButton value="Student" color="red" />
+              </View>
+              <View style={{...styles.radBtn, marginHorizontal: 6}}>
+                <Text>Instructor</Text>
+                <RadioButton value="Instructor" color="red" />
+              </View>
+            </RadioButton.Group>
+            <Button
+              color="black"
+              mode="outlined"
+              style={styles.btn}
+              onPress={() => onRegisterPress()}>
+              Submit
+            </Button>
+          </Card.Content>
+        </Card>
+        <View style={styles.exisitngContainer}>
+          <Text style={{fontSize: 20, textAlign: 'center'}}>
+            Already a user?
+          </Text>
+          <View style={styles.btnContainer}>
+            <Button
+              onPress={() => navigation.navigate('LoginStudent')}
+              style={{borderColor: 'black', borderWidth: 1, borderRadius: 15}}
+              color="black">
+              Login as Student
+            </Button>
+            <Button
+              onPress={() => navigation.navigate('LoginInstructor')}
+              style={{borderColor: 'black', borderWidth: 1, borderRadius: 15}}
+              color="black">
+              Login as Instructor
+            </Button>
+          </View>
+        </View>
+      </View>
+    </ScrollView>
   );
 };
 
@@ -105,7 +119,7 @@ export default Signup;
 
 const styles = StyleSheet.create({
   cardContainer: {
-    marginVertical: 100,
+    marginVertical: 40,
     marginHorizontal: 20,
     elevation: 5,
     borderRadius: 10,
@@ -130,6 +144,10 @@ const styles = StyleSheet.create({
   },
   container: {
     flex: 1,
-    // backgroundColor: '#4dff4d',
+  },
+  btnContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
   },
 });
